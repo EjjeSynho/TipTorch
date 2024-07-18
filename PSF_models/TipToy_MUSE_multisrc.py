New PSF normalization Fixed errors with Cn2 weights Added Tikhonov regularization for LTAO reconstructor

PSF_models/TipToy_MUSE_multisrc.py
@@ -737,15 +737,25 @@ class TipTorch(torch.nn.Module):
         # self.kernel = kernel
         self.C_phi = pdims(kernel, 2) * fix_dims(self.Cn2_weights, self.nL)
 
+        Tikhonov_reg = True
+
         if inv_method == 'standart':
             W_tomo = (self.C_phi @ MP_t) @ torch.linalg.pinv(MP @ self.C_phi @ MP_t + self.C_b, rcond=1e-2)
 
         elif inv_method == 'lstsq':
-            A = (MP @ self.C_phi @ MP_t + self.C_b).transpose(-2, -1)
-            B = (self.C_phi @ MP_t).transpose(-2, -1)
-            # Solve the least squares problem for W^T, since we deal with W*A = B
-            W_tomo = torch.linalg.lstsq(A, B).solution.transpose(-2, -1)
-            
+            if Tikhonov_reg:
+                lambda_reg = 1e-6  # Ridge Regression regularization parameter
+                A = (MP @ self.C_phi @ MP_t + self.C_b).transpose(-2, -1)
+                I_mat = torch.eye(A.size(-1), device=A.device, dtype=A.dtype)
+                A_reg = A.transpose(-2, -1) @ A + lambda_reg * I_mat # Regularized A matrix
+                B = (self.C_phi @ MP_t).transpose(-2, -1)
+                W_tomo = torch.linalg.lstsq(A_reg, A.transpose(-2, -1) @ B).solution.transpose(-2, -1)
+                
+            else:
+                A = (MP @ self.C_phi @ MP_t + self.C_b).transpose(-2, -1)
+                B = (self.C_phi @ MP_t).transpose(-2, -1)
+                # Solve the least squares problem for W^T, since we deal with W*A = B
+                W_tomo = torch.linalg.lstsq(A, B).solution.transpose(-2, -1)           
         else:
             raise ValueError('Unknown inversion method specified.') 
         

data_processing/MUSE_preproc_utils.py
@@ -20,19 +20,21 @@ def LoadMUSEsampleByID(id): # searches for the sample with the specified ID in
     return data_sample
 
 
+def GetRadialBackround(img):
+    from tools.utils import safe_centroid
+    from photutils.profiles import RadialProfile
+    xycen = safe_centroid(img)
+    edge_radii = np.arange(img.shape[-1]//2)
+    rp = RadialProfile(img, xycen, edge_radii)
+    return rp.profile.min()
+
+
 def LoadImages(sample, device=device, convert_images=True):
 
-    def get_bg(img):
-        from tools.utils import safe_centroid
-        from photutils.profiles import RadialProfile
-        xycen = safe_centroid(img)
-        edge_radii = np.arange(img.shape[-1]//2)
-        rp = RadialProfile(img, xycen, edge_radii)
-        return rp.profile.min()
+    PSF_data = np.copy(sample['images']['cube'][:,1:,1:])    
+    bgs = np.array([ GetRadialBackround(PSF_data[i,:,:]) for i in range(PSF_data.shape[0]) ])[:,None,None]
+    PSF_data -= bgs
 
-    PSF_data = np.copy(sample['images']['cube'][:,1:,1:])
-    # bgs = np.array([get_bg(PSF_0_[i,...]) for i in range(PSF_0_.shape[0])])
-    # PSF_0_ = PSF_0_ - bgs[:,None,None]
     norms = PSF_data.sum(axis=(-1,-2), keepdims=True)
     PSF_data /= norms
     
@@ -50,7 +52,7 @@ def LoadImages(sample, device=device, convert_images=True):
     else:
         var_mask = var_mask.astype(np.float32)[None,...]
 
-    return PSF_data, var_mask, norms
+    return PSF_data, var_mask, norms, bgs
 
 
 # 'images'
@@ -105,6 +107,7 @@ def GetConfig(sample, PSF_data, wvl_id=None, device=device, convert_config=True)
         altitudes   = np.array([sample['All data'][f'ALT{i}'].item() for i in range(1, 9)])*100 # in meters
 
         Cn2_weights[Cn2_weights > 1] = 0.0
+        Cn2_weights[Cn2_weights < 0] = 0.0
 
         Cn2_weights_GL = Cn2_weights[altitudes < h_GL]
         altitudes_GL   = altitudes  [altitudes < h_GL]
@@ -208,6 +211,9 @@ def GetConfigSolo(sample, PSF_data, wvl_id=None, device=device, convert_config=T
         Cn2_weights = np.array([sample['All data'][f'CN2_FRAC_ALT{i}'].item() for i in range(1, 9)])
         altitudes   = np.array([sample['All data'][f'ALT{i}'].item() for i in range(1, 9)])*100 # in meters
 
+        Cn2_weights[Cn2_weights > 1] = 0.0
+        Cn2_weights[Cn2_weights < 0] = 0.0
+        
         Cn2_weights_GL = Cn2_weights[altitudes < h_GL]
         altitudes_GL   = altitudes  [altitudes < h_GL]
 
@@ -307,15 +313,15 @@ def rotate_PSF(PSF_0, angle):
 def GetMUSEonsky(ids, derotate_PSF=False, device=device):
     def load_sample(id):
         sample = LoadMUSEsampleByID(id)
-        PSF_0, var_mask, norms = LoadImages(sample, convert_images=False)
+        PSF_0, var_mask, norms, bgs = LoadImages(sample, convert_images=False)
         config_file, PSF_0 = GetConfigSolo(sample, PSF_0, convert_config=False)
-        return PSF_0, var_mask, norms, config_file, sample
+        return PSF_0, var_mask, norms, bgs, config_file, sample
 
     PSF_0 = []
     configs = []
     
     for id in ids:
-        PSF_0_, _, _, config_file_, sample_ = load_sample(id)
+        PSF_0_, _, _, _, config_file_, sample_ = load_sample(id)
         configs.append(config_file_)
         if derotate_PSF:
             PSF_0_rot = rotate_PSF(PSF_0_, -sample_['All data']['Pupil angle'].item())
@@ -333,6 +339,7 @@ def GetMUSEonsky(ids, derotate_PSF=False, device=device):
     merged_config['sources_science']['Wavelength'] = merged_config['sources_science']['Wavelength'][0]
     merged_config['sources_HO']['Height']     = merged_config['sources_HO']['Height'].unsqueeze(-1)
     merged_config['sources_HO']['Wavelength'] = merged_config['sources_HO']['Wavelength'].squeeze()
+    merged_config['NumberSources'] = len(ids)
     
     if derotate_PSF:
         merged_config['telescope']['PupilAngle'] = 0.0

fitting/fitter_MUSE_multichrome.py
@@ -2,16 +2,16 @@
 # %reload_ext autoreload
 # %autoreload 2
 
-import os
 import sys
 sys.path.insert(0, '..')
 
+import os
 import pickle
 import torch
 import numpy as np
 from tools.utils import SR, FitGauss2D, SausageFeature
 from PSF_models.TipToy_MUSE_multisrc import TipTorch
-from data_processing.MUSE_preproc_utils import GetConfig, LoadImages, LoadMUSEsampleByID, rotate_PSF
+from data_processing.MUSE_preproc_utils import GetConfig, LoadImages, LoadMUSEsampleByID, rotate_PSF, GetRadialBackround
 from project_globals import MUSE_DATA_FOLDER, MUSE_FITTING_FOLDER, device
 from tools.config_manager import ConfigManager
 from torchmin import minimize
@@ -40,11 +40,11 @@ else:
     device = default_device
     print("No device specified, using default device.")
 
-if len(sys.argv) == 4:
+if len(sys.argv) > 2:
     start_id, end_id = int(sys.argv[2]), int(sys.argv[3])
 
-if len(sys.argv) == 5:
-    if sys.argv[4] == '-d':   to_float = False
+if len(sys.argv) > 4:
+    if sys.argv[4]   == '-d': to_float = False
     elif sys.argv[4] == '-f': to_float = True
     else:
         print("Unknown option. Use -d for double precision and -f for float precision.")
@@ -57,7 +57,7 @@ psf_df = psf_df[psf_df['Corrupted']   == False]
 psf_df = psf_df[psf_df['Bad quality'] == False]
 
 good_ids = psf_df.index.values.tolist()
-# [103, 240, 259, 264, 296, 319, 349, 367, 407]
+# good_ids = [103, 240, 259, 264, 296, 319, 349, 367, 407]
 
 # If no start or end ID is specified, use the first and last good IDs
 if start_id == -1:
@@ -138,9 +138,10 @@ derotate_PSF = True
 
 #%%
 def load_and_fit_sample(id):
-
+    # id = 240
+    
     sample = LoadMUSEsampleByID(id)
-    PSF_0, _, norms = LoadImages(sample, device)
+    PSF_0, _, norms, bgs = LoadImages(sample, device)
     config_file, PSF_0 = GetConfig(sample, PSF_0, None, device)
     N_wvl = PSF_0.shape[1]
 
@@ -223,9 +224,7 @@ def load_and_fit_sample(id):
     if include_sausage:
         setattr(model, 's_pow', torch.zeros([1,1], device=model.device).float())
 
-
     _ = transformer.stack({ attr: getattr(model, attr) for attr in transformer_dict }) # to create index mapping
-
 
     x0 = [\
         norm_r0.forward(model.r0).item(), # r0
@@ -238,17 +237,16 @@ def load_and_fit_sample(id):
         *([-0.9,]*N_wvl), # Jy
         0.0,              # Jxy
     ]
-
     # PSFAO realm
     if Moffat_absorber:
         x0 += [\
-            *([ 1.0,]*N_wvl),
+            *([ -0.9,]*N_wvl),
             *([-0.5,]*N_wvl),
-            *([ 0.3,]*N_wvl),
+            *([ -0.9,]*N_wvl),
         ]
-
+    # If sausage absorber enabled
     if include_sausage:
-        x0 += [0.9]
+        x0 += [0.5]
 
     x0 = torch.tensor(x0).float().to(device).unsqueeze(0)
 
@@ -280,13 +278,13 @@ def load_and_fit_sample(id):
         return loss_MSE(x_) + loss_MAE(x_) * 0.4
 
 
-    #%% PSF fitting
+    #% PSF fitting
     _ = func(x0)
 
-    result = minimize(loss_MAE, x0, max_iter=100, tol=1e-3, method='bfgs', disp=2)
+    result = minimize(loss_MAE, x0, max_iter=100, tol=1e-3, method='bfgs', disp=0)
     x0 = result.x
 
-    result = minimize(loss_fn, x0, max_iter=100, tol=1e-3, method='bfgs', disp=2)
+    result = minimize(loss_fn, x0, max_iter=100, tol=1e-3, method='bfgs', disp=0)
     x0 = result.x
 
     x_torch = transformer.destack(x0)
@@ -297,7 +295,7 @@ def load_and_fit_sample(id):
         PSF_1 = model(x_torch)
 
 
-    #%%
+    #%
     config_manager = ConfigManager()
     config_manager.Convert(config_file, framework='numpy')
 
@@ -338,12 +336,6 @@ def load_and_fit_sample(id):
     return save_data
 
 #%%
-# filename = MUSE_FITTING_FOLDER + str(id) + '.pickle'
-
-# save_data = load_and_fit_sample(id)
-# if save_data is not None:
-#     with open(filename, 'wb') as handle:
-#         pickle.dump(save_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 for id in good_ids:
     try:

machine_learning/MUSE_fitted_df.py
@@ -6,6 +6,7 @@ import os
 import pickle
 from project_globals import MUSE_DATA_FOLDER
 import pandas as pd
+import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
@@ -52,7 +53,7 @@ for x in data.keys():
     print(x, end=', ')
 
 df_relevant_entries = [
-    'bg', 'F', 'dx', 'dy', 'r0', 'dn', 'Jx', 'Jy', 'Jxy', 'amp', 'b', 'alpha',
+    'bg', 'F', 'dx', 'dy', 'r0', 'dn', 'Jx', 'Jy', 'Jxy', 'amp', 'b', 'alpha', 'sausage_pow',
     'SR data', 'SR fit', 'FWHM fit', 'FWHM data',
 ]
 
@@ -84,10 +85,10 @@ with open(MUSE_DATA_FOLDER + 'MUSE_images_data.pkl', 'wb') as handle:
         'ID': ids
     }
     pickle.dump(images_dict, handle)
-    
+
 #%%
 singular_dict = {}
-for key in ['r0', 'dn', 'Jxy', 'amp', 'b', 'alpha']:
+for key in ['r0', 'dn', 'Jxy', 'amp', 'b', 'alpha', 'sausage_pow']:
     singular_dict[key] = np.squeeze(np.array(fitted_dict_raw[key])).tolist()
 
 Jx_dict = np.squeeze(np.array(fitted_dict_raw['Jx']))
@@ -116,7 +117,6 @@ def make_polychrome_dataset(data):
     return df
 
 singular_df = make_df_from_dict(singular_dict)
-singular_df['r0'] = singular_df['r0'].apply(lambda x: np.abs(x))
 
 Jx_df = make_polychrome_dataset(np.squeeze(np.stack(fitted_dict_raw['Jx'])))
 Jy_df = make_polychrome_dataset(np.squeeze(np.stack(fitted_dict_raw['Jy'])))
@@ -131,11 +131,20 @@ FWHM_fit_y  = make_polychrome_dataset(np.squeeze(np.stack(fitted_dict_raw['FWHM 
 FWHM_data_x = make_polychrome_dataset(np.squeeze(np.stack(fitted_dict_raw['FWHM data'])[...,0]))
 FWHM_data_y = make_polychrome_dataset(np.squeeze(np.stack(fitted_dict_raw['FWHM data'])[...,1]))
 
-FWHM_fit_df  = FWHM_fit_x.apply(lambda x: x**2) + FWHM_fit_y.apply(lambda x: x**2)
+FWHM_fit_df  = FWHM_fit_x.apply(lambda x: x**2)  + FWHM_fit_y.apply(lambda x: x**2)
 FWHM_data_df = FWHM_data_x.apply(lambda x: x**2) + FWHM_data_y.apply(lambda x: x**2)
 
 FWHM_fit_df  = FWHM_fit_df.apply(np.sqrt)
 FWHM_data_df = FWHM_data_df.apply(np.sqrt)
+
+#%%
+# Postprocess dataset
+Jx_df = Jx_df.abs()
+Jy_df = Jy_df.abs()
+singular_df['alpha'] = singular_df['alpha'].apply(lambda x: np.abs(x))
+singular_df['r0']    = singular_df['r0'].apply(lambda x: np.abs(x))
+
+singular_df[singular_df['r0'] > 1] = np.nan
 
 #%%
 # Store dataframes
@@ -177,3 +186,5 @@ plt.title('Polychromatic PSF')
 PSF_avg = lambda x: np.mean(x, axis=1)
 plot_radial_profiles_new( PSF_avg(PSF_0), PSF_avg(PSF_1), 'Data', 'TipTorch', cutoff=40, ax=fig.add_subplot(111) )
 plt.show()
+
+# %%

machine_learning/MUSE_prepare_dataset.py
@@ -36,10 +36,18 @@ muse_df = muse_df[muse_df['Bad quality'] == False]
 
 muse_df_norm = muse_df_norm.loc[muse_df.index]
 
-valid_ids = (test:=fitted_df['dx_df'].mean(axis=1))[~test.isna()].index
+dx_df = fitted_df['dx_df']
+
+valid_ids = (dx_df)[~dx_df.isna()].index.values.tolist()
+exclude_ids = [103, 240, 259, 264, 296, 319, 349, 367, 407]
+
+for id in exclude_ids:
+    if id in valid_ids:
+        valid_ids.remove(id)
 
 muse_df_norm = muse_df_norm.loc[valid_ids]
 muse_df = muse_df.loc[valid_ids]
+
 
 #%%
 PSF_0, merged_config = GetMUSEonsky([100], derotate_PSF=True, device=torch.device('cpu'))
@@ -48,16 +56,20 @@ wvls = (merged_config['sources_science']['Wavelength'].numpy() * 1e9).round(0).a
 column_names = wvls.tolist()
 index_names  = muse_df.index.values
 
-# dx_fitted_df  = pd.DataFrame(np.zeros([len(muse_df.index), len(wvls)]), columns=column_names, index=index_names)
-# dy_fitted_df  = pd.DataFrame(np.zeros([len(muse_df.index), len(wvls)]), columns=column_names, index=index_names)
-# bg_fitted_df  = pd.DataFrame(np.zeros([len(muse_df.index), len(wvls)]), columns=column_names, index=index_names)
-# Jxy_fitted_df = pd.DataFrame(np.zeros([len(muse_df.index), len(wvls)]), columns=column_names, index=index_names)
+F_fitted_df  = fitted_df['F_df'].loc[valid_ids]
+dx_fitted_df = fitted_df['dx_df'].loc[valid_ids]
+dy_fitted_df = fitted_df['dy_df'].loc[valid_ids]
+bg_fitted_df = fitted_df['bg_df'].loc[valid_ids]
+Jx_fitted_df = fitted_df['Jx_df'].loc[valid_ids]
+Jy_fitted_df = fitted_df['Jy_df'].loc[valid_ids]
 
-dx_fitted_df  = fitted_df['dx_df'].loc[valid_ids]
-dy_fitted_df  = fitted_df['dy_df'].loc[valid_ids]
-bg_fitted_df  = fitted_df['bg_df'].loc[valid_ids]
-Jxy_fitted_df = fitted_df['singular_vals_df']['Jxy'].loc[valid_ids]
-
+r0_fitted_df    = fitted_df['singular_vals_df']['r0'].loc[valid_ids]
+Jxy_fitted_df   = fitted_df['singular_vals_df']['Jxy'].loc[valid_ids]
+dn_fitted_df    = fitted_df['singular_vals_df']['dn'].loc[valid_ids]
+amp_fitted_df   = fitted_df['singular_vals_df']['amp'].loc[valid_ids]
+b_fitted_df     = fitted_df['singular_vals_df']['b'].loc[valid_ids]
+alpha_fitted_df = fitted_df['singular_vals_df']['alpha'].loc[valid_ids]
+s_pow_fitted_df = fitted_df['singular_vals_df']['sausage_pow'].loc[valid_ids]
 
 #%% 
 BATCH_SIZE = 4
@@ -78,10 +90,19 @@ def batch_config_and_images(ids):
 def batch_get_data_dicts(ids):
     return { 
         'fitted data': {
-            'Jxy': torch.from_numpy(dx_fitted_df.loc[ids].to_numpy()).float(),
-            'dx':  torch.from_numpy(dx_fitted_df.loc[ids].to_numpy()).float(),
-            'dy':  torch.from_numpy(dy_fitted_df.loc[ids].to_numpy()).float(), 
-            'bg':  torch.from_numpy(bg_fitted_df.loc[ids].to_numpy()).float()   
+            'r0':    torch.from_numpy(r0_fitted_df.loc[ids].to_numpy()).float(),
+            'F':     torch.from_numpy(F_fitted_df.loc[ids].to_numpy()).float(),
+            'dx':    torch.from_numpy(dx_fitted_df.loc[ids].to_numpy()).float(),
+            'dy':    torch.from_numpy(dy_fitted_df.loc[ids].to_numpy()).float(), 
+            'bg':    torch.from_numpy(bg_fitted_df.loc[ids].to_numpy()).float(),
+            'dn':    torch.from_numpy(dn_fitted_df.loc[ids].to_numpy()).float(),
+            'Jx':    torch.from_numpy(Jx_fitted_df.loc[ids].to_numpy()).float(),
+            'Jy':    torch.from_numpy(Jy_fitted_df.loc[ids].to_numpy()).float(),
+            'Jxy':   torch.from_numpy(dx_fitted_df.loc[ids].to_numpy()).float(),
+            'amp':   torch.from_numpy(amp_fitted_df.loc[ids].to_numpy()).float(),
+            'b':     torch.from_numpy(b_fitted_df.loc[ids].to_numpy()).float(),
+            'alpha': torch.from_numpy(alpha_fitted_df.loc[ids].to_numpy()).float(),
+            's_pow': torch.from_numpy(s_pow_fitted_df.loc[ids].to_numpy()).float(),
         },
         'onsky data':  muse_df_norm.loc[ids].to_dict(orient='list')
     }

tests/MUSE_onsky.py
@@ -11,7 +11,7 @@ import pickle
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from tools.utils import plot_radial_profiles_new, SR, draw_PSF_stack, rad2mas, mask_circle
+from tools.utils import plot_radial_profiles_new, SR, draw_PSF_stack, rad2mas, mask_circle, register_hooks
 from PSF_models.TipToy_MUSE_multisrc import TipTorch
 from data_processing.MUSE_preproc_utils import GetConfig, LoadImages, LoadMUSEsampleByID, rotate_PSF
 from tools.config_manager import GetSPHEREonsky
@@ -26,7 +26,7 @@ from tqdm import tqdm
 
 #%%
 
-nanana = [21, 60, 103, 148, 167, 240, 302, 319, 349]
+gave_NaNs = [21, 60, 103, 148, 167, 240, 302, 319, 349]
 # 411, 410, 409, 405, 146, 296, 276, 395, 254, 281, 343, 335
 
 # 21 - fits okay
@@ -35,14 +35,12 @@ nanana = [21, 60, 103, 148, 167, 240, 302, 319, 349]
 # 349 - fits okay, but blurry
 # 302, 319 - wrong rotation, fits okay
 
-
-
 derotate_PSF    = True
 Moffat_absorber = True
 include_sausage = True
 
-sample = LoadMUSEsampleByID(240)
-PSF_0, var_mask, norms = LoadImages(sample)
+sample = LoadMUSEsampleByID(343)
+PSF_0, var_mask, norms, bgs = LoadImages(sample)
 config_file, PSF_0 = GetConfig(sample, PSF_0)
 N_wvl = PSF_0.shape[1]
 
@@ -55,7 +53,7 @@ if derotate_PSF:
 from PSF_models.TipToy_MUSE_multisrc import TipTorch
 from tools.utils import SausageFeature
 
-toy = TipTorch(config_file, 'sum', device, TipTop=True, PSFAO=Moffat_absorber, oversampling=1)
+toy = TipTorch(config_file, 'sum', device, TipTop=True, PSFAO=Moffat_absorber, oversampling=2)
 sausage_absorber = SausageFeature(toy)
 sausage_absorber.OPD_map = sausage_absorber.OPD_map.flip(dims=(-1,-2))
 
@@ -151,7 +149,6 @@ def _radial_profiles(PSFs, centers=None):
     return profiles
 
 
-#%
 
 plt.figure(figsize=(10, 8))
 
@@ -221,8 +218,6 @@ b_pred = reg.coef_ * w + reg.intercept_
 plt.plot(w, a_pred, 'o-', color='blue')
 # plt.plot(w, b, 'o-', color='red')
 plt.plot(w, b_pred, 'o-', color='red')
-
-
 
 
 #%% PSF fitting (no early-stopping)
@@ -373,7 +368,6 @@ x_torch = transformer.destack(x0)
 phase_func = lambda: sausage_absorber(toy.s_pow.flatten()) if include_sausage else None
 PSF_1 = toy(x_torch, None, phase_generator=phase_func)
 
-
 '''
 else:
     _ = func(x0)
@@ -393,7 +387,6 @@ else:
     phase_func = lambda: sausage_absorber(toy.s_pow.flatten()) if include_sausage else None
     PSF_1 = toy(x_torch, None, phase_generator=phase_func)
 '''
-
 
 #%%
 from tools.utils import plot_radial_profiles_new
@@ -417,8 +410,64 @@ else:
     
     plot_radial_profiles_new( PSF_0[0,0,...].cpu().numpy(),  PSF_1[0,0,...].cpu().numpy(),  'Data', 'TipTorch', centers=center, cutoff=60, title='Left PSF')
     plt.show()
+    
+
+#%%
+from tools.utils import calc_profile
+
+x_torch = transformer.destack(x0)
+_ = toy(x_torch, None, phase_generator=phase_func)
+
+dk = 2*toy.kc / toy.nOtf_AO
+
+PSD_norm = lambda wvl: (dk*wvl*1e9/2/np.pi)**2
+
+PSDs = {entry: (toy.PSDs[entry].clone().squeeze() * PSD_norm(500e-9)) for entry in toy.PSD_entries if toy.PSDs[entry].ndim > 1}
+PSDs['chromatism'] = PSDs['chromatism'].mean(dim=0)
+
+#%
+plt.figure(figsize=(8, 6))
+
+PSD_map  = toy.PSD[0,...].mean(dim=0).real.cpu().numpy()
+k_map    = toy.k[0,...].cpu().numpy()
+k_AO_map = toy.k_AO[0,...].cpu().numpy()
+
+center    = [k_map.shape[0]//2, k_map.shape[1]//2]
+center_AO = [k_AO_map.shape[0]//2, k_AO_map.shape[1]//2]
+
+PSD_prof = calc_profile(PSD_map,  center)
+freqs    = calc_profile(k_map,    center)
+freqs_AO = calc_profile(k_AO_map, center_AO)
+
+freq_cutoff = toy.kc.flatten().item()
+
+profiles = {}
+for entry in PSDs.keys():
+    buf_map = PSDs[entry].abs().cpu().numpy()
+    if entry == 'fitting':
+        buf_prof = calc_profile(buf_map, center)
+    else:
+        buf_prof = calc_profile(buf_map, center_AO)
+    profiles[entry] = buf_prof
+
+#%
+plt.plot(freqs, PSD_prof, label='Total', linewidth=2, linestyle='--', color='black')
+
+for entry, value in profiles.items():
+    if entry == 'fitting':
+        plt.plot(freqs, value, label=entry)
+    else:
+        plt.plot(freqs_AO, value, label=entry)
+
+plt.legend()
+plt.yscale('symlog', linthresh=5e-5)
+plt.xscale('log')
+# plt.vlines(freq_cutoff, PSD_prof.min(), PSD_prof.max(), color='red', linestyle='--')
+plt.grid()
+plt.xlim(freqs.min(), freqs.max())
 
 
+#%%
 '''
 PSF_1 = torch.tensor(PSF_1s_).float().to(device).unsqueeze(0)
 PSF_0 = PSF_0_.clone()
@@ -439,6 +488,21 @@ for i in range(N_wvl_):
     plot_radial_profiles_new( A, B,  'Data', 'TipTorch', title=f'{(wvls_[0][i]*1e9):.2f} [nm]')
     plt.savefig(f'C:/Users/akuznets/Desktop/MUSE_fits_new/profiles_{(wvls_[0][i]*1e9):.0f}.png')
 '''
+
+#%%
+x = x0.clone().detach().requires_grad_(True)
+
+x_torch = transformer.destack(x)
+
+Q = ( toy(x_torch)-PSF_0 ).abs().sum()
+get_dot = register_hooks(Q)
+Q.backward()
+dot = get_dot()
+#dot.save('tmp.dot') # to get .dot
+#dot.render('tmp') # to get SVG
+dot # in Jupyter, you can just render the variable
+
+
 #%%
 # plt.plot(toy.wvl.squeeze().cpu().numpy().flatten()*1e9, (toy.Jy**2+toy.Jx**2).sqrt().cpu().numpy().flatten())
 # plt.plot(toy.wvl.squeeze().cpu().numpy().flatten()*1e9, toy.Jy.detach().cpu().numpy().flatten())
@@ -655,38 +719,5 @@ ax[1,2].set_xlabel('Wavelength [nm]')
 
 plt.tight_layout()
 plt.savefig('C:/Users/akuznets/Desktop/MUSE_fits_new/params.png')
-
-#%%
-# for entry in x0_new:
-#     item_ = x0_new[entry]
-    
-#     if len(item_.shape) == 0:
-#         print(f'{entry}: {item_.item():.3f}')
-#     else:
-#         print(entry, end=': ')
-#         for x in item_.cpu().numpy().tolist():
-#             print(f'{x:.3f}',  end=' ')
-#         print('')
-
-
-#%%
-def GetNewPhotons():
-    WFS_noise_var = toy.dn + toy.NoiseVariance(toy.r0.abs())
-
-    N_ph_0 = toy.WFS_Nph.clone()
-
-    def func_Nph(x):
-        toy.WFS_Nph = x
-        var = toy.NoiseVariance(toy.r0.abs())
-        return (WFS_noise_var-var).flatten().abs().sum()
-
-    result_photons = minimize(func_Nph, N_ph_0, method='bfgs', disp=0)
-    toy.WFS_Nph = N_ph_0.clone()
-
-    return result_photons.x
-
-Nph_new = GetNewPhotons()
-
-print(toy.WFS_Nph.item(), Nph_new.item())
 
 

tools/utils.py
@@ -972,6 +972,13 @@ def render_profile(profile, color, label, linestyle='-', linewidth=1, func=lambd
         plt.plot(x, func(profile_m), color=color, label=label, linestyle=linestyle, linewidth=linewidth)
 
 
+def calc_profile(data, xycen=None):
+    xycen = safe_centroid(data) if xycen is None else xycen
+    edge_radii = np.arange(data.shape[-1]//2)
+    rp = RadialProfile(data, xycen, edge_radii)
+    return rp.profile
+
+    
 def plot_radial_profiles_new(PSF_0,
                              PSF_1,
                              label_0 = 'PSFs #1',
@@ -987,12 +994,6 @@ def plot_radial_profiles_new(PSF_0,
                              y_min = 1e-2,
                              suppress_plot = False):
             
-    def calc_profile(data, xycen=None):
-        xycen = safe_centroid(data) if xycen is None else xycen
-        edge_radii = np.arange(data.shape[-1]//2)
-        rp = RadialProfile(data, xycen, edge_radii)
-        return rp.profile
-
     def _radial_profiles(PSFs, centers=None):
         listify_PSF = lambda PSF_stack: [ x.squeeze() for x in np.split(PSF_stack, PSF_stack.shape[0], axis=0) ]
         PSFs = listify_PSF(PSFs)