- Improved Cn2 weights fitting - Fixed Cn2 altitudes computation error in MUSE NFM data routines - Improved P3 & TipTorch comparison routine

PSF_models/NFM_wrapper.py
@@ -15,7 +15,7 @@ from tools.static_phase import ArbitraryBasis, PixelmapBasis, ZernikeBasis, MUSE
 from tools.utils import PupilVLT
 from managers.config_manager import MultipleTargetsInDifferentObservations
 from managers.input_manager  import InputsManager
-from tools.normalizers import Uniform, Uniform0_1
+from tools.normalizers import Uniform, Uniform0_1, Atanh
 from warnings import warn
 from project_settings import device
 import gc
@@ -219,7 +219,15 @@ class PSFModelNFM:
             'diff. refract':   True,
             'Moffat':          self.Moffat_absorber
         }
-        self.model = TipTorch(config, 'LTAO', pupil, PSD_include, 'sum', self.device, oversampling=1)
+        self.model = TipTorch(
+            AO_config = config,
+            AO_type = 'LTAO',
+            pupil = pupil,
+            PSD_include = PSD_include,
+            norm_regime = 'sum',
+            device = self.device,
+            oversampling = 1
+        )
         # _ = self.model()
 
 
@@ -263,22 +271,15 @@ class PSFModelNFM:
         # norm_wind_dir    = Uniform(a=0, b=360)
         # norm_sausage_pow = Uniform(a=0, b=1)
         norm_LO          = Uniform(a=-100, b=100)
-        # norm_GL_h        = Uniform(a=0.0, b=2000.0)
-        # norm_GL_frac     = Atanh()
+        norm_GL_h        = Uniform(a=0.0, b=10000.0)
+        norm_GL_frac     = Atanh()
 
         # Add base parameters
         if self.use_splines:
-
             self.inputs_manager.add('F_ctrl',  torch.tensor([[1.0,]*self.N_spline_ctrl]*N_src),  norm_F)
             self.inputs_manager.add('dx_ctrl', torch.tensor([[0.0,]*self.N_spline_ctrl]*N_src),  norm_dxy)
             self.inputs_manager.add('dy_ctrl', torch.tensor([[0.0,]*self.N_spline_ctrl]*N_src),  norm_dxy)
             self.inputs_manager.add('bg_ctrl', torch.tensor([[0.0,]*self.N_spline_ctrl]*N_src),  norm_bg)
-            
-            # self.inputs_manager.add('F_x_ctrl',  torch.linspace(0, 1, 5, device=self.device).unsqueeze(0))
-            # self.inputs_manager.add('dx_x_ctrl', torch.linspace(0, 1, 5, device=self.device).unsqueeze(0))
-            # self.inputs_manager.add('dy_x_ctrl', torch.linspace(0, 1, 5, device=self.device).unsqueeze(0))
-            # self.inputs_manager.add('bg_x_ctrl', torch.linspace(0, 1, 5, device=self.device).unsqueeze(0))
-                    
         else:
             self.inputs_manager.add('F',  torch.tensor([[1.0,]*N_wvl]*N_src),  norm_F)
             self.inputs_manager.add('dx', torch.tensor([[0.0,]*N_wvl]*N_src),  norm_dxy)
@@ -305,10 +306,11 @@ class PSFModelNFM:
         self.inputs_manager.add('Jxy', torch.tensor([[0.0]]*N_src), norm_Jxy, optimizable=False)
         self.inputs_manager.add('dn',  torch.tensor([0.25]*N_src),  norm_dn)
 
-        # GL_frac = np.maximum(model.Cn2_weights[0,-1].detach().cpu().numpy().item(), 0.9)
-        # GL_h    = model.h[0,-1].detach().cpu().numpy().item()
-        # self.inputs_manager.add('GL_frac', torch.tensor([GL_frac]), norm_GL_frac)
-        # self.inputs_manager.add('GL_h',    torch.tensor([GL_h]), norm_GL_h)
+        GL_frac = self.model.Cn2_weights[:, 0].view(N_src, 1).detach().clone().abs()
+        self.inputs_manager.add('GL_frac', GL_frac, norm_GL_frac)#, optimizable=False)
+        
+        # GL_h = self.model.Cn2_heights[:, -1].view(N_src, 1).detach().clone().abs()
+        # self.inputs_manager.add('GL_h', GL_h, norm_GL_h)#, optimizable=False)
 
         # Add Moffat parameters if needed
         if self.Moffat_absorber:
@@ -379,12 +381,12 @@ class PSFModelNFM:
         if 'wind_speed_single' in x_dict:
             x_dict['wind_speed'] = x_dict['wind_speed_single'].view(-1, 1).repeat(1, self.model.N_L)
 
-        # x_dict['Cn2_weights'] = torch.hstack([x_dict['GL_frac'], 1.0 - x_dict['GL_frac']]).unsqueeze(0)
-        # x_dict['h']           = torch.hstack([torch.tensor([0.0], device=self.device), x_dict['GL_h'].abs()]).unsqueeze(0)
+        x_dict['Cn2_weights'] = torch.hstack([x_dict['GL_frac'].abs(), 1.0 - x_dict['GL_frac'].abs()])
+        # x_dict['Cn2_heights'] = torch.nn.functional.pad(x_dict['GL_h'].abs(), (1, 0), value=0.0)
 
         x_ = { key: x_dict[key] for key in include_list } if include_list is not None else x_dict
 
-        phase_ = lambda: self.phase_func(x_dict['LO_coefs']) if self.LO_NCPAs else None
+        phase_ = (lambda: self.phase_func(x_dict['LO_coefs'])) if self.LO_NCPAs else None
 
         return self.model(x_, None, phase_generator=phase_)
 

PSF_models/TipTorch.py
@@ -85,19 +85,20 @@ class TipTorch(torch.nn.Module):
         self.Cn2_weights = self.config['atmosphere']['Cn2Weights']
         self.Cn2_heights = self.config['atmosphere']['Cn2Heights'] * self.airmass # [m]
         
-        self.stretch  = 1.0 / (1.0 - self.Cn2_heights/self.GS_height)
-        self.h   = self.Cn2_heights * self.stretch
+        # self.h   = self.Cn2_heights * (1.0 / (1.0 - self.Cn2_heights/self.GS_height)) # [N_obs x N_layers]
         self.N_L = self.Cn2_heights.shape[-1]
-
+        
         # N_obs is very important! It must = 1 if all simulated targets share the same observational conditions.
         # Doing so can dramatically reduce the computational cost. Otherwise, for multiple targets in multiple conditions,
         # it must be equal to the number of simulated targets
-        self.N_obs = self.h.shape[0]
+        # self.N_obs = self.h.shape[0]
+        self.N_obs = self.Cn2_heights.shape[0]
+        
         assert self.N_obs == 1 or self.N_obs == self.N_src
 
         # Deformable mirror(s) parameters
         self.pitch = self.config['DM']['DmPitchs'] #[m]
-        self.kc    = 1.0 / (2.0 * self.pitch) # TODO: support multiple DMs, just select biggest pitch among all of them
+        self.kc    = 1.0 / (2.0 * self.pitch) # TODO: support multiple DMs, just select the biggest pitch among all of them
         
         self.h_DM  = self.config['DM']['DmHeights'] # [m]
         self.N_DM  = self.h_DM.shape[0]
@@ -387,7 +388,7 @@ class TipTorch(torch.nn.Module):
         
         # PyTorch doesn't support interpolation of complex tensors yet
         OTF_ = self.interp(OTF.real, self.sampling_min) + self.interp(OTF.imag, self.sampling_min)*1j
-        return OTF_ / OTF_.abs().amax(dim=(-2,-1), keepdim=True)
+        return OTF_ / OTF_.abs().amax(dim=(-2,-1), keepdim=True) # normalize OTF
     
     
     def ComputeStaticOTF(self, phase_generator=None):
@@ -508,7 +509,10 @@ class TipTorch(torch.nn.Module):
         
         if    self.norm_regime == 'sum': self.normalizer = torch.sum
         elif  self.norm_regime == 'max': self.normalizer = torch.amax
-        else: self.normalizer = lambda x, dim, keepdim: self.make_tensor(1.0)
+        else: self.normalizer = lambda x, dim, keepdim: x # no normalization
+        
+        # Default inversion method for tomographic reconstruction
+        self.inversion_method = 'lstsq'
         
         # Piston filters           
         self.piston_filter = None # piston mode filter in the AO-corrected freq. domain
@@ -547,7 +551,10 @@ class TipTorch(torch.nn.Module):
     # TODO: allow to input the atmospheric params externaly to account user-assumed atmospheric layers instead of simulated ones
     def OptimalDMProjector(self):
         h_dm = self.h_DM.view(1, 1, 1, self.N_DM)
-        h    = self.h.view(self.N_obs, 1, 1, 1, self.N_L)
+        # h    = self.h.view(self.N_obs, 1, 1, 1, self.N_L)
+        stretch = 1.0 / (1.0 - self.Cn2_heights/self.GS_height)
+        h = (self.Cn2_heights * stretch).view(self.N_obs, 1, 1, 1, self.N_L)
+        
         opt_w = self.DM_opt_weight.view(self.N_obs, 1, 1, self.N_optdir, 1, 1)
 
         theta_x = self.DM_opt_dir_x.view(self.N_obs, 1, 1, self.N_optdir)
@@ -700,8 +707,10 @@ class TipTorch(torch.nn.Module):
         else:
             kx = pdims(self.kx_AO, 1)
             ky = pdims(self.ky_AO, 1)
-            h  = self.h.view(self.N_obs, 1, 1, self.N_L)
-            
+            # h  = self.h.view(self.N_obs, 1, 1, self.N_L)
+            stretch = 1.0 / (1.0 - self.Cn2_heights/self.GS_height)
+            h = (self.Cn2_heights * stretch).view(self.N_obs, 1, 1, self.N_L)
+        
             beta_x = self.src_dirs_x.view(self.N_src, 1, 1, 1)
             beta_y = self.src_dirs_y.view(self.N_src, 1, 1, 1)
             
@@ -782,7 +791,10 @@ class TipTorch(torch.nn.Module):
 
     def DifferentialRefractionPSD(self):
         # TODO: account for the pupil angle
-        h = self.h.view(self.N_obs, 1, 1, 1, self.N_L)
+        # h = self.h.view(self.N_obs, 1, 1, 1, self.N_L)
+        stretch = 1.0 / (1.0 - self.Cn2_heights/self.GS_height)
+        h = (self.Cn2_heights * stretch).view(self.N_obs, 1, 1, 1, self.N_L)
+        
         w = self.Cn2_weights.view(self.N_obs, 1, 1, 1, self.N_L)
         k = self.k_AO.view(1, 1, self.nOtf_AO_y, self.nOtf_AO_x, 1)
         # [N_src x 1 x nOtf_AO_y x nOtf_AO_x]
@@ -790,7 +802,7 @@ class TipTorch(torch.nn.Module):
         # [N_src x N_wvl]
         tan_theta = torch.tan((self.IOR_src_wvl - pdims(self.IOR_GS_wvl, 1)) * torch.tan(self.zenith_angle))
         
-        return self.W_atm.unsqueeze(1) * ( 2*w*(1-torch.cos(2*torch.pi*h*k * pdims(tan_theta,3) * pdims(cos_ang,1))) ).sum(dim=-1)
+        return self.W_atm.unsqueeze(1) * ( 2*w*(1-torch.cos(2*torch.pi*h*k * pdims(tan_theta,3) * pdims(cos_ang,1))) ).sum(dim=-1) * self.mask_corrected_AO
     
 
     def JitterKernel(self, Jx, Jy, Jxy):
@@ -825,34 +837,6 @@ class TipTorch(torch.nn.Module):
 
         return varNoise
 
-    '''
-    def NoiseVariance_new(self, WFS): #TODO: fix excessive CPU->GPU copying
-        WFS_wvl = WFS['wvl']
-        WFS_Nph = WFS['Nph'].abs().view(self.N_src, self.N_GS)
-        r0_WFS = r0_new(self.r0.view(self.N_src), WFS_wvl, self.wvl_atm).abs()
-        WFS_nPix = WFS['FOV'] / WFS['n_sub']
-        WFS_pixelScale = WFS['psInMas'] / 1e3  # [arcsec]
-
-        # Read-out noise calculation
-        nD = torch.maximum(
-            self.rad2arc * WFS_wvl / WFS['d_sub'] / WFS_pixelScale, 
-            self.make_tensor(1.0)
-        )  # spot FWHM in pixels without turbulence
-
-        # Photon-noise calculation
-        nT = torch.maximum(
-            torch.hypot(self.WFS_spot_FWHM.max() / 1e3, self.rad2arc * WFS_wvl / r0_WFS) / WFS_pixelScale,
-            self.make_tensor(1.0)
-        )
-
-        varRON  = np.pi**2 / 3 * (WFS['RON']**2 / WFS_Nph**2) * (WFS_nPix**2 / nD).unsqueeze(-1)**2
-        varShot = np.pi**2 / (2*WFS_Nph) * (nT/nD).unsqueeze(-1)**2
-
-        # Noise variance calculation
-        varNoise = WFS['excessive_factor'] * (varRON + varShot) * (self.wvl_atm / WFS_wvl).unsqueeze(-1)**2
-
-        return varNoise
-    '''
 
     def TomographicReconstructors(self, WFS_noise_var, inv_method='lstsq'):
         '''        
@@ -860,7 +844,9 @@ class TipTorch(torch.nn.Module):
         then it's possible to compute one tomographic reconstructor for all simulated sources.
         For example, this is the case when all objects are within one FoV and belong to one observation
         '''
-        h = self.h.view(self.N_obs, 1, 1, 1, self.N_L)
+        # h = self.h.view(self.N_obs, 1, 1, 1, self.N_L)
+        stretch = 1.0 / (1.0 - self.Cn2_heights/self.GS_height)
+        h = (self.Cn2_heights * stretch).view(self.N_obs, 1, 1, 1, self.N_L)
         
         kx = pdims(self.kx_AO, 2)
         ky = pdims(self.ky_AO, 2)
@@ -886,7 +872,7 @@ class TipTorch(torch.nn.Module):
 
         # Inversion happens relative to the last two dimensions of the these tensors
         if inv_method == 'standart':
-            self.W_tomo = (self.C_phi @ MP_t) @ torch.linalg.pinv(MP @ self.C_phi @ MP_t + self.C_b, rcond=1e-2)
+            self.W_tomo = (self.C_phi @ MP_t) @ torch.linalg.pinv(MP @ self.C_phi @ MP_t + self.C_b, rcond=1e-3) #1e-4
 
         elif inv_method == 'lstsq':
             # if Tikhonov_reg:
@@ -978,7 +964,7 @@ class TipTorch(torch.nn.Module):
             self.ReconstructionFilter(WFS_noise_var)
                         
             if self.tomography:
-                self.TomographicReconstructors(WFS_noise_var, inv_method='lstsq')
+                self.TomographicReconstructors(WFS_noise_var, inv_method=self.inversion_method)
                 if not self.on_axis:
                     self.DMProjector()
 
@@ -1009,6 +995,7 @@ class TipTorch(torch.nn.Module):
         #TODO: anisoplanatism for non-tomography!
         #TODO: SLAO support!
         
+        # Resulting dimensions are: [N_scr x N_wvl x nOtf_AO x nOtf_AO]
         PSD = self.PSDs['fitting'] + self.PSD_padder(
               self.PSDs['WFS noise'] + \
               self.PSDs['spatio-temporal'] + \
@@ -1016,12 +1003,14 @@ class TipTorch(torch.nn.Module):
               self.PSDs['chromatism'] + \
               self.PSDs['Moffat'] + \
               self.PSDs['diff. refract'])
-        # Resulting dimensions are: [N_scr x N_wvl x nOtf_AO x nOtf_AO]
+      
+        # Removing the DC component
+        PSD[..., self.nOtf_y//2, self.nOtf_x-1] = 0.0
       
         # All PSDs are computed in [rad^2] at the atmospheric wvls and then normalized to [nm^2] OPD at tscience wvl
         PSD_norm = (self.dk*self.wvl_atm*1e9/2/torch.pi)**2
         # Recover the full-size PSD from the half-sized one
-        self.PSD = self.half_PSD_to_full(PSD * PSD_norm) # [nm^2]
+        self.PSD = self.half_PSD_to_full(PSD * PSD_norm) # [nm^2]    
         
         return self.PSD
     
@@ -1076,9 +1065,6 @@ class TipTorch(torch.nn.Module):
         Jy  = pdims(min_2d(self.Jy ), 2)
         Jxy = pdims(min_2d(self.Jxy), 2)
                 
-        # Removing the DC component
-        PSD[..., self.nOtf_y//2, self.nOtf_x-1] = 0.0
-        
         # Computing OTF from PSD, real is to remove the imaginary part that appears due to numerical errors
         cov = self._rfft2_to_full(torch.fft.fftshift(torch.fft.rfft2(torch.fft.ifftshift(PSD.abs(), dim=(-2,-1)), dim=(-2,-1)), dim=-2).real)
 

data_processing/MUSE_STD_dataset_utils.py
@@ -57,7 +57,7 @@ def LoadSTDStarData(
         if ensure_odd_pixels:
             if PSF_data.shape[-1] % 2 == 0:
                 PSF_data = PSF_data[:,:-1,:-1]
-                PSF_STD  = PSF_STD[:,:-1,:-1]
+                PSF_STD  = PSF_STD [:,:-1,:-1]
         
         if subtract_background:
             backgrounds = np.array([ get_radial_backround(PSF_data[i,:,:]) for i in range(PSF_data.shape[0]) ])[:,None,None]
@@ -111,12 +111,7 @@ def LoadSTDStarData(
     norms = torch.tensor(np.array(norms), dtype=default_torch_type, device=device)
     bgs   = torch.tensor(np.array(bgs),   dtype=default_torch_type, device=device)
 
-    # merged_config = MultipleTargetsInDifferentObservations(configs, device=device)
-
-    # if derotate_PSF:
-        # merged_config['telescope']['PupilAngle'] = 0.0 # Meaning, that the PSF is already derotated
-
-    return PSFs, norms, bgs, configs #merged_config
+    return PSFs, norms, bgs, configs 
 
 
 def RenameMUSECubes(folder_cubes_old, folder_cubes_new):
@@ -269,4 +264,44 @@ def update_label_IDs():
     # Show a concise dataframe summary
     df = pd.DataFrame(results, columns=["original_name","new_name","matched_folder_file","index_changed","labels"])
     df.head()
+
+
+def AOF_Cn2_profiles_stats(STD_stars_df, store=False):
+    # Compute median Cn2 profile for MUSE NFM
+    all_Cn2_alts, all_Cn2_fracs = [], []
+
+    for idx in STD_stars_df.index:
+        Cn2_alt_sample  = [STD_stars_df[f'ALT{i}'].loc[idx].item()          for i in range(1, 9)]
+        Cn2_frac_sample = [STD_stars_df[f'CN2_FRAC_ALT{i}'].loc[idx].item() for i in range(1, 9)]
+        all_Cn2_alts.append(Cn2_alt_sample)
+        all_Cn2_fracs.append(Cn2_frac_sample)
+
+    all_Cn2_alts  = np.array(all_Cn2_alts)
+    all_Cn2_fracs = np.array(all_Cn2_fracs)
+
+    # Filter out unreasonable values
+    all_Cn2_alts [all_Cn2_alts > 100] = np.nan
+    all_Cn2_alts [all_Cn2_alts < 0]   = np.nan
+    all_Cn2_fracs[all_Cn2_fracs > 1]  = np.nan
+    all_Cn2_fracs[all_Cn2_fracs < 0]  = np.nan
+    all_Cn2_alts[:,-1][all_Cn2_alts[:,-1] < 1e-12] = np.nan
+
+    # Compute median and percentiles (ignoring NaNs)
+    median_Cn2_alts  = np.nanmedian(all_Cn2_alts, axis=0)
+    p16_Cn2_alts     = np.nanpercentile(all_Cn2_alts, 16, axis=0)
+    p84_Cn2_alts     = np.nanpercentile(all_Cn2_alts, 84, axis=0)
+
+    median_Cn2_fracs = np.nanmedian(all_Cn2_fracs, axis=0)
+    p16_Cn2_fracs    = np.nanpercentile(all_Cn2_fracs, 16, axis=0)
+    p84_Cn2_fracs    = np.nanpercentile(all_Cn2_fracs, 84, axis=0)
     
+    if store:
+        import json
+        Cn2_profile_dict = {
+            'median_Cn2_alts':  median_Cn2_alts.tolist(),
+            'median_Cn2_fracs': median_Cn2_fracs.tolist()
+        }
+        with open(DATA_FOLDER / 'reduced_telemetry/MUSE/AOF_median_Cn2_profile.json', 'w') as json_file:
+            json.dump(Cn2_profile_dict, json_file)
+
+    return (median_Cn2_alts, median_Cn2_fracs), (p16_Cn2_alts, p84_Cn2_fracs), (p16_Cn2_fracs, p84_Cn2_alts)

data_processing/MUSE_data_utils.py
@@ -27,8 +27,7 @@ from pathlib import Path
 from tools.utils import GetROIaroundMax, GetJmag, check_framework
 from tools.network import DownloadFromRemote
 from tools.plotting import wavelength_to_rgb
-from managers.parameter_parser import ParameterParser
-from managers.config_manager import ConfigManager, MultipleTargetsInDifferentObservations
+from managers.config_manager import ConfigManager
 from scipy.ndimage import rotate
 import logging
 
@@ -1770,32 +1769,83 @@ def InitNFMConfig(sample, PSF_data=None, wvl_ids=None, device=device, convert_co
 
     wvls_ = (sample['spectral data']['wvls binned']*1e-9).tolist()
     wvls  = wvls_ if wvl_ids is None else np.array([wvls_[i] for i in wvl_ids])
-        
-    # Load the default MUSE NFM config file
+
+
+    # >>>>>>> Load the default MUSE NFM config file
     config_manager = ConfigManager()
     config_dict = config_manager.Load(DATA_FOLDER / 'parameter_files/muse_ltao.ini')
 
-    h_GL = 2000 # [m], an assumed separation altitude between the ground and high-altitude layers
-    # For NFM it's save to assume gound layer to be below 2 km, for WFM it's even lower than that
 
-    try:
-        Cn2_weights = np.array([sample['All data'][f'CN2_FRAC_ALT{i}'].item() for i in range(1, 9)])
-        altitudes   = np.array([sample['All data'][f'ALT{i}'].item() for i in range(1, 9)])*100 # in meters
+    # >>>>>>> Compute simplified Cn2 profile
+    # Read median Cn2 profile from the json file
+    with open(DATA_FOLDER / 'reduced_telemetry/MUSE/AOF_median_Cn2_profile.json', 'r') as f:
+        median_Cn2_profile = json.load(f)
+    
+    median_Cn2_alts  = np.array(median_Cn2_profile['median_Cn2_alts'])
+    median_Cn2_fracs = np.array(median_Cn2_profile['median_Cn2_fracs'])
+    
+    Cn2_alt  = np.array([sample['All data'][f'ALT{i}'].item()          for i in range(1, 9)])
+    Cn2_frac = np.array([sample['All data'][f'CN2_FRAC_ALT{i}'].item() for i in range(1, 9)])
 
-        Cn2_weights[Cn2_weights > 1] = 0.0
-        Cn2_weights[Cn2_weights < 0] = 0.0
+    # Check if all NaN, if yes, replace with median profile
+    if np.all(np.isnan(Cn2_alt)) or np.all(np.isnan(Cn2_frac)) or sample['Raw Cn2 data'] is None:
+        print(f"Warning: Cn2 profile data is missing or invalid. Replacing with the median profile.")
+        Cn2_alt  = median_Cn2_alts
+        Cn2_frac = median_Cn2_fracs
 
-        Cn2_weights_GL = Cn2_weights[altitudes < h_GL]
-        altitudes_GL   = altitudes  [altitudes < h_GL]
+    # Remove unphysical values
+    Cn2_alt [Cn2_alt > 100] = np.nan
+    Cn2_alt [Cn2_alt < 0]   = np.nan
+    Cn2_frac[Cn2_frac > 1]  = np.nan
+    Cn2_frac[Cn2_frac < 0]  = np.nan
+    Cn2_alt[-1] = np.nan if Cn2_alt[-1] < 1e-12 else Cn2_alt[-1]
 
-        GL_frac  = Cn2_weights_GL.sum()  # Ground layer fraction   
-        Cn2_w_GL = np.interp(h_GL, altitudes, Cn2_weights)
-        
-    except:
-        GL_frac = 0.9
+    # Replace NaN values with median ones
+    nan_mask_alt = np.isnan(Cn2_alt)
+    Cn2_alt[nan_mask_alt] = median_Cn2_alts[nan_mask_alt]
 
-    config_dict['NumberSources'] = 1
+    # make sure all altitudes are increasing
+    for i in range(1,8):
+        Cn2_alt[i] = np.maximum(Cn2_alt[i], Cn2_alt[i-1])
 
+    nan_mask_frac = np.isnan(Cn2_frac)
+    Cn2_frac[nan_mask_frac] = 0.0
+
+    h_GL = 20.0 # [km], this is not the ground layer, but rather an optimal separation height
+
+    Cn2_w_GL = np.interp(h_GL, Cn2_alt, Cn2_frac)
+
+    Cn2_alt_lower = np.concatenate((Cn2_alt[Cn2_alt < h_GL], [h_GL]))
+    Cn2_alt_upper = np.concatenate(([h_GL], Cn2_alt[Cn2_alt >= h_GL]))
+
+    Cn2_frac_lower = np.concatenate((Cn2_frac[Cn2_alt < h_GL], [Cn2_w_GL]))
+    Cn2_frac_upper = np.concatenate(([Cn2_w_GL], Cn2_frac[Cn2_alt >= h_GL]))
+
+    # Compute fraction below GL
+    total_integral    = np.trapezoid(Cn2_frac, Cn2_alt)
+    fraction_below_GL = np.trapezoid(Cn2_frac_lower, Cn2_alt_lower) / total_integral
+    fraction_above_GL = np.trapezoid(Cn2_frac_upper, Cn2_alt_upper) / total_integral
+
+    if fraction_below_GL > 1-1e-6:
+        print("Warning: Cn2 fraction below GL is ~1. Adjusting to avoid numerical issues.")
+        fraction_below_GL = np.float64(0.99)
+        fraction_above_GL = np.float64(0.01)
+
+    plt.plot(Cn2_frac, Cn2_alt, 'o-')
+    plt.scatter(Cn2_w_GL, h_GL, color='red', label='GL Cn2 fraction')
+    plt.axhline(h_GL, color='gray', linestyle='--', label='GL Height')
+    plt.xlim(-0.01, 1)
+    plt.ylim(0, 30)
+    plt.xlabel('Cn2 Fraction')
+    plt.ylabel('Altitude (km)')
+    plt.title(f'AOF Cn2 profile')
+    plt.legend()
+
+    print(f"Fraction of Cn2 below GL (h≤{h_GL} km): {fraction_below_GL:.4f}")
+    print(f"Fraction of Cn2 above GL (h>{h_GL} km): {fraction_above_GL:.4f}")
+
+
+    # >>>>>>> Modify the config file according to the data for this particular sample
     config_dict['telescope']['TelescopeDiameter'] = 8.0
     config_dict['telescope']['ZenithAngle'] = [90.0 - sample['MUSE header data']['Tel. altitude'].item()]
     config_dict['telescope']['Azimuth']     = [sample['MUSE header data']['Tel. azimuth'].item()]
@@ -1808,24 +1858,26 @@ def InitNFMConfig(sample, PSF_data=None, wvl_ids=None, device=device, convert_co
         except (KeyError, TypeError):
             config_dict['telescope']['PupilAngle'] = 0.0
     
-    
     if sample['Raw Cn2 data'] is not None:
         config_dict['atmosphere']['L0']  = [sample['All data']['L0Tot'].item()]
     else:
         config_dict['atmosphere']['L0']  = [config_dict['atmosphere']['L0']]
                 
     config_dict['atmosphere']['Seeing'] = [sample['MUSE header data']['Seeing (header)'].item()]
-    config_dict['atmosphere']['Cn2Weights'] = [[GL_frac, 1-GL_frac]]
-    config_dict['atmosphere']['Cn2Heights'] = [[0, h_GL]]
+
+    config_dict['atmosphere']['Cn2Heights'] = [[0.0, h_GL*1e3]]  # convert to [m]
+    config_dict['atmosphere']['Cn2Weights'] = [[fraction_below_GL.item(), fraction_above_GL.item()]]
 
     config_dict['atmosphere']['WindSpeed']     = [[sample['MUSE header data']['Wind speed (header)'].item(),]*2]
     config_dict['atmosphere']['WindDirection'] = [[sample['MUSE header data']['Wind dir (header)'].item(),]*2]
     config_dict['sources_science']['Wavelength'] = wvls
 
-    config_dict['sources_LO']['Wavelength'] = (1215 + 1625)/2.0 * 1e-9 # Mean  approx. wavelengths between J and H
+    config_dict['sources_LO']['Wavelength'] = (1215 + 1625)/2.0 * 1e-9 # Mean approx. wavelengths of IRLOS between J and H
+    config_dict['sources_HO']['Zenith']     = [[10,]*4] # [arcsec], LGSs angular separation from on-axis
 
     config_dict['sensor_science']['PixelScale'] = sample['MUSE header data']['Pixel scale (science)'].item()
     config_dict['sensor_science']['FieldOfView'] = FoV_science
+
 
     try:
         LGS_ph = np.array([sample['All data'][f'LGS{i} photons, [photons/m^2/s]'].item() / 1240e3 for i in range(1,5)])
@@ -1834,9 +1886,10 @@ def InitNFMConfig(sample, PSF_data=None, wvl_ids=None, device=device, convert_co
     except:
         LGS_ph = [[2000,]*4]
         
-    config_dict['sensor_HO']['NumberPhotons']  = LGS_ph
-    config_dict['sensor_HO']['SizeLenslets']   = config_dict['sensor_HO']['SizeLenslets'][0]
-    config_dict['sensor_HO']['NumberLenslets'] = config_dict['sensor_HO']['NumberLenslets'][0]
+    config_dict['sensor_HO']['NumberPhotons'] = LGS_ph
+    config_dict['sensor_HO']['ClockRate']     = np.mean([config_dict['sensor_HO']['ClockRate']])
+    # config_dict['sensor_HO']['SizeLenslets']   = config_dict['sensor_HO']['SizeLenslets'][0]
+    # config_dict['sensor_HO']['NumberLenslets'] = config_dict['sensor_HO']['NumberLenslets'][0]
     # config_file['sensor_HO']['NoiseVariance'] = 4.5
 
     IRLOS_ph_per_subap_per_frame = sample['IRLOS data']['IRLOS photons (cube), [photons/s/m^2]'].item()
@@ -1856,9 +1909,9 @@ def InitNFMConfig(sample, PSF_data=None, wvl_ids=None, device=device, convert_co
 
     # config_file['DM']['DmPitchs'] = [config_file['DM']['DmPitchs'][0]*1.25]
     config_dict['DM']['DmPitchs'][0] = 0.22
-
-    config_dict['sensor_HO']['ClockRate'] = np.mean([config_dict['sensor_HO']['ClockRate']])
+        
     
+    # >>>>>>> Finalize the config file
     config_dict = config_manager.wrap_scalars_to_lists(config_dict)
     config_dict = config_manager.ensure_dimensions(config_dict, N_src=1) # Config for a single given source
     

tests/MUSE_onsky.py
@@ -8,26 +8,21 @@ sys.path.insert(0, '..')
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
 import matplotlib as mpl
-
+from matplotlib.colors import LogNorm
 from torchmin import minimize
 
 from tools.plotting import plot_radial_PSF_profiles, draw_PSF_stack, plot_chromatic_PSF_slice
 from tools.utils import GradientLoss
-
 from data_processing.MUSE_STD_dataset_utils import STD_FOLDER, LoadSTDStarData
-from project_settings import device
-
 from PSF_models.NFM_wrapper import PSFModelNFM
+from project_settings import device
 
 #%%
 import pickle
 
 with open(STD_FOLDER / 'muse_df.pickle', 'rb') as handle:
     muse_df = pickle.load(handle)
-    
-# muse_df.index[muse_df['Wind speed (header)'] > 16]
 
 #%%
 # wvl_ids = np.clip(np.arange(0, (N_wvl_max:=30)+1, 5), a_min=0, a_max=N_wvl_max-1)
@@ -54,7 +49,7 @@ wvl_ids = np.clip(np.arange(0, (N_wvl_max:=30)+1, 2), a_min=0, a_max=N_wvl_max-1
 # ids = 184 # weak wind patterns
 # ids = 338 # blurry
 # ids = 470 # blurry
-# ids = 346 # blurry
+# ids = 346 # blurry (very)
 # ids = 206 # blurry, good for red debugging
 # ids = 179 # blurry
 # ids = 174
@@ -78,17 +73,12 @@ wvl_ids = np.clip(np.arange(0, (N_wvl_max:=30)+1, 2), a_min=0, a_max=N_wvl_max-1
 
 # ids = 482 # good one
 # ids = 494 # good one
-# ids = 462 # good one
-# ids = 475 # good one
+ids = 462 # good one
+# ids = 475 # good one, mb DM correction radius mismatch
 # ids = 468 # good one
 
-ids = 455 # Interesting tomo reconst behaviour
-
-# ids = 449
-# ids = 440
-
-# ids = [455, 456]
-ids = 456
+# ids = 455 # large tomographic error (?)
+# ids = 456 # large tomographic error
 
 PSF_0, norms, bgs, configs = LoadSTDStarData(
     ids                 = ids,
@@ -112,7 +102,6 @@ PSF_model = PSFModelNFM(
     device          = device
 )
 
-#%%
 func = lambda x_: PSF_model( PSF_model.inputs_manager.unstack(x_) )
 PSF_1 = func(x0 := PSF_model.inputs_manager.stack() )
 
@@ -132,6 +121,8 @@ for j in range(N_src):
         im = PSF_0[j,i,...].cpu().numpy()
         vmin = np.percentile(im[im > 0], 10) if np.any(im > 0) else 1
         vmax = np.percentile(im[im > 0], 99.975) if np.any(im > 0) else im.max()
+        # vmin = np.percentile(im[im > 0], 97) if np.any(im > 0) else 1
+        # vmax = np.percentile(im[im > 0], 99.95) if np.any(im > 0) else im.max()
 
         plt.imshow(im, cmap=cmap, norm=LogNorm(vmin=vmin, vmax=vmax))
         plt.axis('off')
@@ -139,10 +130,8 @@ for j in range(N_src):
         
     plt.show()
 
-#%
 # plot_chromatic_PSF_slice(PSF_0, wavelengths, norms, window_size=40)
 # plot_chromatic_PSF_slice(PSF_0, wavelengths, window_size=100, slices=['horizontal'])
-
 
 #%%
 from tools.static_phase import ArbitraryBasis, PixelmapBasis, ZernikeBasis
@@ -177,16 +166,21 @@ def loss_fn(x_, w_MSE, w_MAE):
 
     return MSE_loss + MAE_loss + LO_loss + Moffat_loss_fn()
 
+suppress_sausage = False
+suppress_LO = False
+
+suppress_sausage = 1e3 if suppress_sausage else 1
+suppress_LO = 1e3 if suppress_LO else 1
 
 def loss_LO_fn():
     if isinstance(PSF_model.LO_basis, PixelmapBasis):
-        LO_loss = grad_loss_fn(PSF_model.OPD_func.unsqueeze(1)) * 5e-5
+        # LO_loss = grad_loss_fn(PSF_model.OPD_func.unsqueeze(1)) * 5e-5
+        raise NotImplementedError("Gradient loss for PixelmapBasis is not implemented yet.")
         
     elif isinstance(PSF_model.LO_basis, ZernikeBasis) or isinstance(PSF_model.LO_basis, ArbitraryBasis):
-        LO_loss = PSF_model.inputs_manager['LO_coefs'].pow(2).sum(-1).mean() * 1e-7
-        # print(LO_loss)
+        LO_loss = PSF_model.inputs_manager['LO_coefs'].pow(2).sum(-1).mean() * 1e-7 * suppress_LO
         # Constraint to enforce first element of LO_coefs to be positive
-        first_coef_penalty = torch.clamp(-PSF_model.inputs_manager['LO_coefs'][:, 0], min=0).pow(2).mean() * 5e-5
+        first_coef_penalty = torch.clamp(-PSF_model.inputs_manager['LO_coefs'][:, 0], min=0).pow(2).mean() * 5e-5 * suppress_sausage
         LO_loss += first_coef_penalty
         
     return LO_loss
@@ -273,7 +267,7 @@ def minimize_params(loss_fn, include_list, exclude_list, max_iter, verbose=True)
     result = lim_lambda('l-bfgs', 1e-4)
     
     # Retry with BFGS if convergence issues detected
-    if result['nit'] < max_iter * 0.3 or result['fun'] > 1:
+    if result['nit'] < max_iter * 0.3 or result['fun'] > 1: # If converged earlier than 30% of the iterations or final loss is too high
         if verbose:
             reason = "stopped too early" if result['nit'] < max_iter * 0.3 else "final loss is high"
             print(f"Warning: minimization {reason}. Perhaps, convergence wasn't reached? Trying BFGS...")
@@ -288,7 +282,6 @@ def minimize_params(loss_fn, include_list, exclude_list, max_iter, verbose=True)
 
     return result.x, func(result.x), OPD_map
 
-
 fit_wind_speed = True
 fit_outerscale = True
 
@@ -297,7 +290,8 @@ include_general = ['r0', 'dn'] + \
                   (['LO_coefs'] if PSF_model.LO_NCPAs else []) + (['chrom_defocus'] if PSF_model.chrom_defocus else []) + \
                   ([x+'_ctrl' for x in PSF_model.polychromatic_params] if PSF_model.use_splines else PSF_model.polychromatic_params) + \
                   (['L0'] if fit_outerscale else []) + \
-                  (['wind_speed_single'] if fit_wind_speed else [])
+                  (['wind_speed_single'] if fit_wind_speed else []) #+ \
+                #   ['GL_h_c']
                 #   ([x+'_x_ctrl' for x in PSF_model.polychromatic_params] if PSF_model.use_splines else PSF_model.polychromatic_params) + \
 
 exclude_general = ['ratio', 'theta', 'beta'] if PSF_model.Moffat_absorber else []
@@ -356,6 +350,7 @@ if PSF_model.LO_NCPAs:
     plt.colorbar()
     plt.show()
 
+
 #%%
 import torch.nn.functional as F
 
@@ -370,7 +365,7 @@ torch.cuda.empty_cache()
 #%%
 center_y, center_x = PSF_0.shape[-2] // 2, PSF_0.shape[-1] // 2
 half_crop = H_orig // 2
-ROI = slice(center_y - half_crop, center_y + half_crop)
+ROI = slice( center_y - half_crop, center_y + half_crop+(PSF_0.shape[0] % 2) )
 
 # Downsampling factor to reduce memory usage
 pool_size = 3
@@ -417,10 +412,15 @@ for k, v in J_per_param.items():
     # sensitivity_map[k] = res_vec * J_per_param[k]
     sensitivity_map[k] = J_per_param[k]
 
+#%%
+
+# List all shapes of sensitivity maps
+for k, v in J_per_param.items():
+    print(f"{k:15s}: {v.shape}")
 
 #%%
 
-studied_param = 'L0'
+studied_param = 'dn'
 
 final_map = sensitivity_map[studied_param][0,...,0].cpu().detach().numpy()
 final_map -= final_map.mean()

tests/p3_PSD_comparison.py
@@ -2,12 +2,13 @@
 %reload_ext autoreload
 %autoreload 2
 
-from logging import config
 import sys
 import os
 import tempfile
-
-from cv2 import merge
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import matplotlib.cm as cm
 
 sys.path.append('..')
 
@@ -19,7 +20,6 @@ for module in ['MASTSEL', 'P3', 'SEEING', 'SYMAO', 'TIPTOP']:
     sys.path.append(str(TIPTOP_PATH:=(PROJECT_PATH / '../astro-tiptop') / f'{module}/{module.lower()}'))
 
 sys.path.append(str(TIPTOP_PATH:=(PROJECT_PATH / '../astro-tiptop')))
-# from P3.p3.aoSystem.fourierModel import fourierModel
 from aoSystem.fourierModel import fourierModel
 
 # Verify paths
@@ -59,74 +59,192 @@ tiptop_HO_model = fourierModel(
 os.remove(temp_path_ini)
 
 #%%
-# Compute different PSD contributors
-p3_fitting  = tiptop_HO_model.fittingPSD().squeeze()
-p3_aliasing = tiptop_HO_model.aliasingPSD().squeeze()
-p3_diff_ref = tiptop_HO_model.differentialRefractionPSD().squeeze()
-p3_chromatism = tiptop_HO_model.chromatismPSD().squeeze()
-p3_spatioTemporal = tiptop_HO_model.spatioTemporalPSD().squeeze()
-p3_noise = tiptop_HO_model.noisePSD().squeeze()
+# Compute different P3 PSD contributors and pack into dictionary
+P3_PSDs = {
+    'fitting':         tiptop_HO_model.fittingPSD().squeeze().copy(),
+    'aliasing':        tiptop_HO_model.aliasingPSD().squeeze().copy(),
+    'diff. refract':   tiptop_HO_model.differentialRefractionPSD().squeeze().copy(),
+    'chromatism':      tiptop_HO_model.chromatismPSD().squeeze().copy(),
+    'spatio-temporal': tiptop_HO_model.spatioTemporalPSD().squeeze().copy(),
+    'WFS noise':       tiptop_HO_model.noisePSD().squeeze().copy()
+}
 
 #%%
-%reload_ext autoreload
-%autoreload 2
+# %reload_ext autoreload
+# %autoreload 2
 
 from PSF_models.TipTorch import TipTorch
-from managers.config_manager import ConfigManager, MultipleTargetsInOneObservation
-# from managers.parameter_parser import ParameterParser
-from project_settings import device, DATA_FOLDER
-# import numpy as np
-from pprint import pprint
-# import torch
-# import cupy as cp
+from managers.config_manager import ConfigManager
+from project_settings import device, DATA_FOLDER, default_torch_type
 
 #%%
-path_ini = DATA_FOLDER / "parameter_files/muse_ltao.ini"
-
+# path_ini = DATA_FOLDER / "parameter_files/muse_ltao.ini"
 config_manager = ConfigManager()
 config_dict = config_manager.Load(path_ini)
-
-#%%
-common_dict = MultipleTargetsInOneObservation(config_dict, N_srcs=10, device=device)
-debug_dims = config_manager.debug_dims(common_dict)
-
-#%%
-merged_dict = config_manager.Merge([config_dict,]*3)
-merged_dict = config_manager.Convert(merged_dict, 'pytorch', device=device)
-debug_dims = config_manager.debug_dims(merged_dict)
-
-# MultipleTargetsInDifferentObservations([config_dict,]*3, device=device)
-
-#%%
-# TODO:
-# - fix default pupil loading errors
-# - compute noise variance for every WFS separately and then average it
-# - make function to preserve only necessary entries in configs
-
-# pupil = torch.tensor(PupilVLT(samples=320, rotation_angle=0)).to(device)
-
-# Define which PSDs to include
-PSD_include = {
-    'fitting':         True,
-    'WFS noise':       True,
-    'spatio-temporal': True,
-    'aliasing':        True,
-    'chromatism':      True,
-    'diff. refract':   True,
-    'Moffat':          False
-}
+config_dict = config_manager.Convert(config_dict, framework='pytorch', device=device, dtype=default_torch_type)
 
 # Initialize TipTorch model
 tiptorch_model = TipTorch(
-    AO_config=common_dict,
+    AO_config=config_dict,
     AO_type='LTAO',
-    PSD_include=PSD_include,
     norm_regime='sum',
     device=device,
     oversampling=1
 )
 
-#%%
+# Update oversampling to match P3 model exactly
+match_sampling = (tiptop_HO_model.freq.k_.min().get() / tiptorch_model.sampling_factor.item()) * 1.001
+tiptorch_model.oversampling = match_sampling
+tiptorch_model.Update(init_grids=True, init_pupils=True, init_tomography=True)
+
 PSF_1 = tiptorch_model()
 
 #%%
+def PSD_preprocess(psd_data):
+    """ Convert both PSDs to numpy arrays and get real part. Handles half PSD for TipTorch, too. """
+    if hasattr(psd_data, 'cpu'):
+        PSD_buf = tiptorch_model.half_PSD_to_full(psd_data).squeeze().cpu().numpy().real
+        PSD_buf[PSD_buf.shape[0]//2, PSD_buf.shape[1]//2] = 0.0
+        return PSD_buf[:-1,:-1]
+    elif hasattr(psd_data, 'get'): # Convert from Cupy to Numpy
+        return psd_data.squeeze().get().real
+    else:
+        return np.array(psd_data).squeeze().real # Numpy array already
+
+TipTorch_PSDs = {}
+for key in P3_PSDs.keys():
+    TipTorch_PSDs[key] = PSD_preprocess(tiptorch_model.PSDs[key].clone())
+    P3_PSDs[key] = PSD_preprocess(P3_PSDs[key])
+
+# print(P3_PSDs['fitting'].shape)
+# print(TipTorch_PSDs['fitting'].shape)
+
+#%%
+def display_PSD(im, title=None, cmap='viridis', show_axis=True, fontsize=12, ax=None, vmin=None, vmax=None):
+    """ Display a PSD with log normalization. Returns axis object and image for colorbar creation. """
+    
+    # Use provided vmin/vmax or calculate percentiles for log normalization
+    if vmin is None or vmax is None:
+        vmin = np.percentile(im[im > 0], 1e1)    if np.any(im > 0) else 1
+        vmax = np.percentile(im[im > 0], 99.975) if np.any(im > 0) else im.max()
+    
+    im_plot = ax.imshow(im, cmap=cmap, norm=LogNorm(vmin=vmin, vmax=vmax))
+    
+    if not show_axis:
+        ax.axis('off')
+    else:
+        ax.tick_params(axis='both', which='major', labelsize=fontsize)
+    
+    if title: ax.set_title(title, fontsize=fontsize)
+    
+    return ax, im_plot
+
+
+# Iterate over all PSD contributors and create 1x2 subplots for each
+for key in P3_PSDs.keys():
+    if key in TipTorch_PSDs:
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6))
+                
+        torch_data = TipTorch_PSDs[key]
+        p3_data = P3_PSDs[key]
+        
+        # Calculate common vmin/vmax from both datasets
+        combined_positive = np.concatenate([p3_data[p3_data > 0].flatten(), torch_data[torch_data > 0].flatten()])
+        vmin = np.percentile(combined_positive, 10)
+        vmax = np.percentile(combined_positive, 99.975)
+        
+        # Plot P3 PSD on the left
+        ax1, im1 = display_PSD(P3_PSDs[key], title=f'P3 {key} PSD', ax=ax1, vmin=vmin, vmax=vmax)
+        # Plot TipTorch PSD on the right
+        ax2, im2 = display_PSD(TipTorch_PSDs[key], title=f'TipTorch {key} PSD', ax=ax2, vmin=vmin, vmax=vmax)
+        # Add shared colorbar - thicker and on the right
+        cbar = fig.colorbar(im2, ax=[ax1, ax2], label='PSD Value', fraction=0.03, pad=0.02, aspect=15, shrink=1.8)
+        cbar.ax.tick_params(labelsize=12)
+        
+        plt.tight_layout()
+        plt.subplots_adjust(right=0.8)
+        plt.show()
+
+#%%
+# Calculate and plot normalized difference maps (as percentages)
+for key in P3_PSDs.keys():
+    if key in TipTorch_PSDs:
+        fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+        
+        torch_data = TipTorch_PSDs[key]
+        p3_data = P3_PSDs[key]
+        
+        # Calculate percentage difference: (TipTorch - P3) / P3 * 100
+        # Avoid division by zero by adding small epsilon where P3 is zero
+        epsilon = 1e-15
+        diff_normalized = ((torch_data - p3_data) / (p3_data + epsilon)) * 100
+        
+        # Set reasonable limits for the difference plot
+        vmin = np.percentile(diff_normalized, 2.5)
+        vmax = np.percentile(diff_normalized, 97.5)
+        
+        # Plot the normalized difference
+        im = ax.imshow(diff_normalized, cmap='RdBu_r', vmin=vmin, vmax=vmax)
+        ax.set_title(f'Normalized Difference: {key} PSD\n(TipTorch - P3) / P3 x 100%', fontsize=12)
+        
+        # Add colorbar
+        cbar = fig.colorbar(im, ax=ax, label='Percentage Difference (%)', fraction=0.046, pad=0.04)
+        cbar.ax.tick_params(labelsize=10)
+        
+        plt.tight_layout()
+        plt.show()
+
+#%%
+diffs = {}
+
+# Print error statistics
+for key in P3_PSDs.keys():
+    if key in TipTorch_PSDs:
+        torch_data = TipTorch_PSDs[key]
+        p3_data    = P3_PSDs[key]
+        
+        epsilon = 1e-15 
+        diff_normalized = ((torch_data - p3_data) / (p3_data + epsilon)) * 100 # [%]
+        diffs[key] = diff_normalized.copy()
+        
+        mean_diff = np.mean(diff_normalized)
+        std_diff  = np.std(diff_normalized)
+        max_diff  = np.max(np.abs(diff_normalized))
+        
+        print(f"{key}: Mean: {mean_diff:.2f}%, STD: {std_diff:.2f}%, Max: {max_diff:.2f}%")
+
+# %%
+from photutils.profiles import RadialProfile
+
+def plot_PSD_radial_profile(img, title, linestyle='-', color=None):
+    xycen = (img.shape[-1]//2, img.shape[-2]//2)
+    edge_radii = np.arange(img.shape[-1]//2)
+    rp = RadialProfile(img, xycen, edge_radii)
+
+    spatial_freq = rp.radius * tiptorch_model.dk.cpu().numpy()
+    plt.plot(spatial_freq, rp.profile, linestyle=linestyle, color=color, label=title, linewidth=1)
+
+    # plt.ylabel(rf'PSD [nm$^2$/(1/m)$^2$]')
+    plt.xscale('symlog', linthresh=5e-2)
+
+    plt.xlim(1e-1, 1e1)
+
+    plt.grid(True, which='both', alpha=0.3)
+    plt.legend()
+    plt.xlabel('Spatial frequency (1/m)')
+    
+    
+plt.figure(figsize=(12,8))
+
+plot_PSD_radial_profile(P3_PSDs['fitting'], 'P3 fitting', linestyle='--', color='C0')
+plot_PSD_radial_profile(TipTorch_PSDs['fitting'], 'TipTorch fitting', linestyle='-', color='C0')
+
+plot_PSD_radial_profile(P3_PSDs['WFS noise'], 'P3 WFS noise', linestyle='--', color='C1')
+plot_PSD_radial_profile(TipTorch_PSDs['WFS noise'], 'TipTorch WFS noise', linestyle='-', color='C1')
+
+plt.title('PSD Radial Profile Comparison')
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+# %%

tools/utils.py
@@ -144,8 +144,11 @@ def cropper(x, win, center=None):
 def safe_centroid(data):       
     xycen = centroid_quadratic(np.abs(data))
     
-    if np.any(np.isnan(xycen)): xycen = centroid_com(np.abs(data))
-    if np.any(np.isnan(xycen)): xycen = np.array(data.shape)//2
+    if np.any(np.isnan(xycen)):
+        xycen = centroid_com(np.abs(data))
+        
+    if np.any(np.isnan(xycen)):
+        xycen = np.array(data.shape)//2
     
     return xycen
 